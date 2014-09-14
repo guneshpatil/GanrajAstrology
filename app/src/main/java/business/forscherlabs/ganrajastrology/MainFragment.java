50116791fb88dc463d067427601e05edeb4c1d90
guneshpatil <guneshpatil@gmail.com>
2014-09-15 01:06:01 +0530

    first commit after code review

---
+++ app/src/main/java/business/forscherlabs/ganrajastrology/MainFragment.java
@@ -0,0 +1,35 @@
+package business.forscherlabs.ganrajastrology;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+/**
+ * Created by Gunesh on 9/14/2014.
+ */
+public class MainFragment extends Fragment {
+    public MainFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        Button btn_about = (Button) rootView.findViewById(R.id.btn_about);
+        btn_about.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent abt_intent = new Intent(getActivity(), AboutActivity.class);
+                startActivity(abt_intent);
+            }
+        });
+
+        return rootView;
+    }
+
+}
